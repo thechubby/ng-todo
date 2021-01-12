8a364f528813e27911e6583e32695d5d7308ff33
Christine <31845754+thechubby@users.noreply.github.com>
2021-01-12 17:11:31 +0300

    ToDo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-notes';
+  notes = ['Заметка'];
+  input: string;
+  add(item): void {
+    this.notes.push(item);
+  }
+  del(item): void {
+    this.notes.splice(item, 1);
+  }
 }
