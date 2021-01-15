cefaf3c8781c409b781204bae2adb91574bd46d8
Christine <31845754+thechubby@users.noreply.github.com>
2021-01-15 16:59:48 +0300

    Working on localStorage

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {FormControl, Validators} from '@angular/forms';
 
 @Injectable({providedIn: NotesService})
@@ -8,19 +8,23 @@ export class NotesService {
   public input = new FormControl(null, [Validators.required]);
   public sbj = new BehaviorSubject<Array<string>>([]);
 
-/*
   constructor() {
-    localStorage.setItem('key', JSON.stringify(notes))
+    localStorage.getItem('key');
   }
-*/
 
-  add(): void {
+  private store(): void {
+    localStorage.setItem('key', JSON.stringify(this.sbj.value));
+  }
+
+  public add(): void {
     this.sbj.next(this.sbj.value.concat([this.input.value]));
+    this.store();
   };
 
-  del(iter: number) {
+  public del(iter: number) {
     const newNotes: string[] = [...this.sbj.value];
     newNotes.splice(iter, 1)
     this.sbj.next(newNotes);
+    this.store();
   }
 };
